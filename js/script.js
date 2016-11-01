ba4f76d40a69eb4994eceaaa57da7591f5330a60
Andrei Bondar <a.y.bondar@gmail.com>
2016-11-01 18:00:19 +0300

    fix(all): chnage description, delete portfolio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-var links = document.querySelectorAll(".navigation__link");
+(function () {
+	var links = document.querySelectorAll(".js-nav-link");
 
-//каждой ссылке добавляем addEventListener
-for (i = 0; i < links.length; i++) {
-	links[i].addEventListener("click", function() {
-		document.querySelector(".navigation__link--active").classList.remove("navigation__link--active"); //удаляем класс у активной ссылки
-		this.classList.add("navigation__link--active"); //добавляем класс ссылке, на которую щелкнули
-	})
-};
+	for (i = 0; i < links.length; i++) {
+		links[i].addEventListener("click", function (e) {
+			var target = e.currentTarget;
+			document.querySelector(".navigation__link--active").classList.remove("navigation__link--active");
+			target.classList.add("navigation__link--active");
+		})
+	};
+
+})();
